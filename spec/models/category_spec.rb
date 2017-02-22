1636014b564470451f50033f026ab216c76306b9
Edilene <edilene_cruz@yahoo.com.br>
2017-02-22 14:38:52 -0700

    updates category model and adds donations model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it { should validate_presence_of(:name) }
+  it { should have_many(:donations) }
 end
--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -5,26 +5,5 @@ RSpec.describe Donation, type: :model do
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:image_path) }
-
-  it "can calculate the total number of items it holds" do
-    cart = Cart.new({"1" => 2, "2" => 3})
-
-    expect(cart.total_count).to eq(5)
-  end
-
-  it "can add a donation to its contents" do
-    cart = Cart.new({"1" => 1})
-
-    cart.add_donation(1)
-    cart.add_donation(2)
-
-    expect(cart.contents).to eq({"1" => 2, "2" => 1})
-  end
-
-  it "can report on how many of a particular donation it has" do
-    cart = Cart.new({ "1" => 3, "2" => 1 })
-
-    expect(cart.count_of(1)).to eq(3)
-  end
-
+  it { should belong_to(:category) }
 end
