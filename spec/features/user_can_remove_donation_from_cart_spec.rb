bdf434ce4ca16f503e9617795093be1018f3bc45
Katy Farmer <dreadpiratekaty@gmail.com>
2017-02-22 22:09:23 -0700

    Moves closing paren to fix style violation

--- spec/features/user_can_remove_donation_from_cart_spec.rb
+++ spec/features/user_can_remove_donation_from_cart_spec.rb
@@ -12,7 +12,8 @@ feature "user can remove item from cart" do
 
     expect(current_path).to eq(cart_index_path)
     expect(page).to have_content(
-      "Successfully removed #{donation.title} from your cart.")
+      "Successfully removed #{donation.title} from your cart."
+      )
     expect(page).to have_link(donation.title, href: donation_path(donation))
     expect(page).to_not have_css("#donation-#{donation.id}")
   end
