e6171c392c6ec7c1d57d057831b0a16afaab9260
Ashley Schauer <aelschauer@gmail.com>
2017-02-23 17:29:18 -0700

    updates OrdersController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,26 @@
 class OrdersController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:order_id])
+  end
+
+  def create
+    # order = Order.new(username: "Rachel")
+    # cart.contents.each do |donation_id, quantity|
+    #   order.donations.new(donation_id: donation_id, quantity: quantity)
+    # end
+
+    if order.save
+      session[:cart] = nil
+      flash[:notice] = "Your cart is ready. You kindly chose to give #{cart.donations.count} donations."
+      redirect_to donations_path
+    else
+      # implement if you have validations
+    end
+  end
 end
