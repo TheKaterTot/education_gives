6e4f0ccccaabaaaccb65c38340f52198077ec7b2
Amara Lovato <amaracuenca@gmail.com>
2017-02-23 17:28:54 -0700

    visitor dont see cheeckout in cart

---
+++ spec/features/visitor_dont_see_checkout_in_cart_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "Visitor doesn't see checkout in cart" do
+  scenario "visitor cannot see checkout in cart" do
+    donation = create(:donation)
+    visit donation_path(donation)
+
+    click_on "Add to Cart"
+
+    click_on "Cart: 1"
+
+    expect(current_path).to eq cart_index_path
+    expect(page).to have_content("#{donation.title}")
+    expect(page).to have_selector(:link_or_button, "Login")
+    expect(page).to have_selector(:link_or_button, "Register to Checkout")
+
+    click_on "Register to Checkout"
+
+    expect(current_path).to eq new_user_path
+
+    fill_in("First name", with: "Jane")
+    fill_in("Last name", with: "Doe")
+    fill_in("Email", with: "janedoe@email.com")
+    fill_in("Username", with: "janedoe")
+    fill_in("Password", with: "password")
+
+    click_on "Create User"
+
+    expect(current_path).to eq dashboard_path
+
+    click_on "Cart: 1"
+
+    expect(current_path).to eq cart_index_path
+    expect(page).to have_content("#{donation.title}")
+
+    click_on "Logout"
+
+    expect(current_path).to eq login_path
+
+    expect(page).to have_selector(:link_or_button, "Login")
+    expect(page).to_not have_selector(:link_or_button, "Logout")
+  end
+
+end
