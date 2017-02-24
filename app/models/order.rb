b2c732fa7d78b41a8e546e15c9754a24f37e33e8
Ashley Schauer <aelschauer@gmail.com>
2017-02-24 12:21:44 -0700

    adds method to display the create date for the order that's more human readable

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :details, class_name: "OrderDonation"
   has_many :donations, through: :details
+
+  def display_create_date
+    created_at.strftime("%m-%d-%Y")
+  end
 end
