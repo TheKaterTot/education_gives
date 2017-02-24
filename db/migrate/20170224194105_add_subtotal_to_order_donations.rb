c740fda1dd7f885c9b9e44d3786224e2a95b9c2b
Ashley Schauer <aelschauer@gmail.com>
2017-02-24 12:50:21 -0700

    Change subtotal from integer to float and add subtotal to seed

--- db/migrate/20170224194105_add_subtotal_to_order_donations.rb
+++ db/migrate/20170224194105_add_subtotal_to_order_donations.rb
@@ -1,5 +1,5 @@
 class AddSubtotalToOrderDonations < ActiveRecord::Migration[5.0]
   def change
-    add_column :order_donations, :subtotal, :integer
+    add_column :order_donations, :subtotal, :float
   end
 end
