63ce9b3ae93c33e884850eef4e019990c5ab5946
Katy Farmer <dreadpiratekaty@gmail.com>
2017-02-24 17:25:24 -0700

    Removes explicit join creation from orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,20 +14,13 @@ class OrdersController < ApplicationController
     @cart.contents.each do |donation_id, quantity|
       donation = Donation.find(donation_id)
       order.donations << donation
-      make_order_donation(donation_id, order.id, quantity)
     end
     if order.save
       session[:cart] = nil
       flash[:notice] = "Order was successfully placed."
       redirect_to orders_path
     else
-      # implement if you have validations
+      redirect_to cart_index_path
     end
-  end
-
-  private
-
-  def make_order_donation(donation_id, order_id, quantity)
-    OrderDonation.create(donation_id: donation_id, order_id: order_id, quantity: quantity)
   end
 end
