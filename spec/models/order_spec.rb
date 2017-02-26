44262364691fedc48ab82f81fafc011e9b28dbdb
Katy Farmer <dreadpiratekaty@gmail.com>
2017-02-25 20:03:42 -0700

    Updates order model tests and deletes empty test file

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,7 +1,69 @@
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
+describe Order do
   it { should belong_to(:user) }
   it { should have_many(:details) }
   it { should have_many(:donations) }
+
+  describe "#cancellable" do
+    context "when it is paid" do
+      it "returns true" do
+        order = create(:order, status: "paid")
+
+        expect(order.cancellable?).to be true
+      end
+    end
+
+    context "when it is ordered" do
+      it "returns true" do
+        order = create(:order, status: "ordered")
+
+        expect(order.cancellable?).to be true
+      end
+    end
+
+    context "when it is completed" do
+      it "returns false" do
+        order = create(:order, status: "completed")
+
+        expect(order.cancellable?).to be false
+      end
+    end
+
+    context "when it is cancelled" do
+      it "returns false" do
+        order = create(:order, status: "cancelled")
+
+        expect(order.cancellable?).to be false
+      end
+    end
+  end
+
+  describe "#completed" do
+    context "when it is completed" do
+      it "returns true" do
+        order = create(:order, status: "completed")
+
+        expect(order.completed?).to be true
+      end
+    end
+
+    context "when it is not completed" do
+      it "returns false" do
+        order = create(:order, status: "paid")
+
+        expect(order.completed?).to be false
+      end
+    end
+  end
+
+  describe "#cancel" do
+    it "cancels the order" do
+      order = create(:order, status: "paid")
+      order.cancel
+
+      expect(order.reload.status).to eq("cancelled")
+    end
+  end
+
 end
