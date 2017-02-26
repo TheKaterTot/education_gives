f65d9a205bdd3eafa4d9b81eefc9679bc02c92f3
Katy Farmer <dreadpiratekaty@gmail.com>
2017-02-25 19:34:38 -0700

    Adds ability to transition status from admin dashboard

--- app/models/order.rb
+++ app/models/order.rb
@@ -16,17 +16,28 @@ class Order < ApplicationRecord
   end
 
   def display_status
-    status.capitalize
+    status.capitalize if status
   end
 
   def next_status
     if status == "ordered"
-      ["paid", "cancelled"]
+      "paid"
     elsif status == "paid"
-      ["completed", "cancelled"]
-    else
-      []
+      "completed"
     end
+  end
+
+  def completed?
+    status == "completed"
+  end
+
+  def cancellable?
+    ["paid", "ordered"].include?(status) && status != "cancelled"
+  end
+
+  def cancel
+    self.status = "cancelled"
+    save
   end
 
   def self.count_by_status(status)
