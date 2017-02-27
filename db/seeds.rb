644b9a7a1276c2010cb1fa3a408b7f2fe38a76ec
Amara Lovato <amaracuenca@gmail.com>
2017-02-27 09:35:52 -0700

    adds manual creation of admin in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ library.donations.create(title: "Library Books",
                          price: 50.0,
                          image_path: "#")
 
-admin = FactoryGirl.create(:user, role: 1, username: "admin", password: "password")
+admin = User.create(first_name: "Maria", last_name: "Smith", role: 1, username: "admin", password: "password")
 
 10.times do
   f = Faker::Name.first_name
