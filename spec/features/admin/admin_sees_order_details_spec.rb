ebc6a63bcadae0c9e9a648399127c917f9820dbe
Amara Lovato <amaracuenca@gmail.com>
2017-02-27 17:00:25 -0700

    adds draft feature test for user can see order details

---
+++ spec/features/admin/admin_sees_order_details_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "User sees order details" do
+  scenario "user can see order details" do
+    order_donation = create(:order_donation)
+
+    visit order_path(order_donation.order_id)
+
+    expect(page).to have_content(order_donation.order.created_at)
+    # expect(page).to have_content(order_donation.order.user_id.username) #doesn't work
+    # expect(page).to have_content(order_donation.order.user_id.email) #doesn't work
+    # expect(page).to have_content(order_donation.order_id.title) #doesn't work
+    # expect(page).to have_content(order_donation.donation_id.price) #doesn't work
+    expect(page).to have_content(order_donation.subtotal)
+    # expect(page).to have_content(order_donation.order_id.total) #doesn't work
+    # expect(page).to have_content(order_donation.order_id.status) #doesn't work
+  end
+end
